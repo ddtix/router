d241fd9650d5f5e55302e26efa767b61485ab5eb
ddtix <262985729+ddtix@users.noreply.github.com>
2026-03-05 19:08:06 +0300

    BASE_PATH

--- src/Route.php
+++ src/Route.php
@@ -2,10 +2,35 @@
 
 namespace Ddtix\Router;
 
+use Controllers\Test;
+
 class Route
 {
-    public static function get(string $path): string
+    public static function load(): void
     {
-        return $path;
+        spl_autoload_register(function ($className) {
+            $parts = explode('\\', $className);
+            $fileName = array_pop($parts);
+            $folders = implode('/', $parts);
+            $filePath = getenv('BASE_PATH') . '/../src/' . $folders . '/' . $fileName . '.php';
+
+            if (file_exists($filePath)) {
+                require_once $filePath;
+            }
+        });
+
+        if (isset($_SERVER['REQUEST_URI']) && is_string($_SERVER['REQUEST_URI'])) {
+
+            $url = explode('/', $_SERVER['REQUEST_URI']);
+            $connector = 'Controllers\\' . ucfirst($url[1]);
+            $result = $connector::index();
+
+            echo match (true) {
+                is_string($result), is_numeric($result) => $result,
+                is_array($result), is_object($result) => json_encode($result, JSON_UNESCAPED_UNICODE),
+                is_bool($result) => $result ? 'true' : 'false',
+                default => '',
+            };
+        }
     }
 }
